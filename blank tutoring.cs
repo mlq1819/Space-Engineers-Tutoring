2a95c469883f3f8fb4f0259648fba00698bacae3
Michael Quinn <mquinn5@binghamton.edu>
2021-01-12 15:05:07 -0500

    created a template program to create assignments from

---
+++ blank tutoring.cs
@@ -0,0 +1,160 @@
+const string Program_Name = ""; //Name me!
+Color DEFAULT_TEXT_COLOR=new Color(197,137,255,255); //I determine the color of the text! (RGBA)
+Color DEFAULT_BACKGROUND_COLOR=new Color(44,0,88,255); //I determine the color of the screen! (RGBA)
+
+
+class Tutorial{
+/*
+ * This is a class.
+ * You can put class global variables here that can be used by any method in the Tutorial Class.
+ * Their values are saved between uses, but not between edits of the code.
+ * You can also create more methods here.
+*/
+	
+	
+	
+	
+/*
+ * This method will be called every time the programmable block is run.
+ * The Main method is the "Main" entry point into the program, and should contain your logic
+ * The "argument" is the words you put as the programmable block's argument
+*/
+	public void Tutorial_Main(string argument){
+		
+	}
+
+/*
+ * This method will be called every time the programmable block compiles, which happens during a recompile, and when the world starts up.
+ * The Program method is your "setup" method, used to initialize objects and variables you will be using multiple times.
+*/
+	public void Tutorial_Program(){
+		
+	}
+
+/*
+ * This method will be called every time the programmable block saves, which happens during a recompile, and when the world saves.
+ * The Save method is your "save" method, used to help the Program method next time that runs.
+ * For example, you might want to save some data by writing data to the Storage field, which is saved when the world is closed. Then you could reference the data in Storage in Program to setup.
+*/
+	public void Tutorial_Save(){
+		
+	}
+	
+/*
+ * This is the "Print" Method
+ * It will output your information to the screen of the program
+ * Use it by calling Print with a string
+ * For example, calling "Print("Hello World");" will print the phrase "Hello World" to the programmable block
+*/
+	public static void Print(string input){
+		Prog.Write(input);
+	}
+	
+	public string Storage{
+		get{
+			return Prog.P.Storage;
+		}
+		set{
+			Prog.P.Storage=value;
+		}
+	}
+}
+
+//Don't worry about code down here; it all helps make this easier for you
+
+public Program(){
+	Prog.P=this;
+	Me.CustomName=(Program_Name+" Programmable block").Trim();
+	for(int i=0;i<Me.SurfaceCount;i++){
+		Me.GetSurface(i).FontColor=DEFAULT_TEXT_COLOR;
+		Me.GetSurface(i).BackgroundColor=DEFAULT_BACKGROUND_COLOR;
+		Me.GetSurface(i).Alignment=TextAlignment.CENTER;
+		Me.GetSurface(i).ContentType=ContentType.TEXT_AND_IMAGE;
+	}
+	Me.GetSurface(1).FontSize=2.2f;
+	Me.GetSurface(1).TextPadding=40.0f;
+	MyTutorial=new Tutorial();
+	MyTutorial.Tutorial_Program();
+}
+
+public void Save(){
+    MyTutorial.Tutorial_Save();
+}
+
+void UpdateProgramInfo(){
+	cycle=(++cycle)%long.MaxValue;
+	switch(loading_char){
+		case '|':
+			loading_char='\\';
+			break;
+		case '\\':
+			loading_char='-';
+			break;
+		case '-':
+			loading_char='/';
+			break;
+		case '/':
+			loading_char='|';
+			break;
+	}
+	Write("",false,false);
+	Echo(Program_Name+" OS Cycle-"+cycle.ToString()+" ("+loading_char+")");
+	Me.GetSurface(1).WriteText(Program_Name+" OS Cycle-"+cycle.ToString()+" ("+loading_char+")",false);
+	seconds_since_last_update=Runtime.TimeSinceLastRun.TotalSeconds + (Runtime.LastRunTimeMs / 1000);
+	Echo(ToString(FromSeconds(seconds_since_last_update))+" since last cycle");
+	Time_Since_Start=UpdateTimeSpan(Time_Since_Start,seconds_since_last_update);
+	Echo(ToString(Time_Since_Start)+" since last reboot\n");
+	Me.GetSurface(1).WriteText("\n"+ToString(Time_Since_Start)+" since last reboot",true);
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+	UpdateProgramInfo();
+	MyTutorial.Tutorial_Main(argument);
+}
+
+TimeSpan FromSeconds(double seconds){
+	return (new TimeSpan(0,0,0,(int)seconds,(int)(seconds*1000)%1000));
+}
+
+TimeSpan UpdateTimeSpan(TimeSpan old,double seconds){
+	return old+FromSeconds(seconds);
+}
+
+string ToString(TimeSpan ts){
+	if(ts.TotalDays>=1)
+		return Math.Round(ts.TotalDays,2).ToString()+" days";
+	else if(ts.TotalHours>=1)
+		return Math.Round(ts.TotalHours,2).ToString()+" hours";
+	else if(ts.TotalMinutes>=1)
+		return Math.Round(ts.TotalMinutes,2).ToString()+" minutes";
+	else if(ts.TotalSeconds>=1)
+		return Math.Round(ts.TotalSeconds,3).ToString()+" seconds";
+	else 
+		return Math.Round(ts.TotalMilliseconds,0).ToString()+" milliseconds";
+}
+
+void Write(string text,bool new_line=true,bool append=true){
+	Echo(text);
+	if(new_line)
+		Me.GetSurface(0).WriteText(text+'\n', append);
+	else
+		Me.GetSurface(0).WriteText(text, append);
+}
+
+TimeSpan Time_Since_Start=new TimeSpan(0);
+long cycle=0;
+char loading_char='|';
+double seconds_since_last_update=0;
+Tutorial MyTutorial;
+
+public class Prog{
+	public static MyGridProgram P;
+	public static void Write(string text,bool new_line=true,bool append=true){
+		P.Echo(text);
+		if(new_line)
+			P.Me.GetSurface(0).WriteText(text+'\n', append);
+		else
+			P.Me.GetSurface(0).WriteText(text, append);
+	}
+}
